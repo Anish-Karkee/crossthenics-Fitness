72bab82a96fe8409aaa2442fd238bc5152580dbf
Anish-Karkee <anitakarkey7@gmail.com>
2026-06-15 20:07:03 +0545

    fix: adjust button margin in Hero section and remove unnecessary padding in Service section

--- src/components/sections/Hero.tsx
+++ src/components/sections/Hero.tsx
@@ -34,7 +34,7 @@ export default function Hero() {
             </p>
           </div>
           <Link href="/allproducts">
-            <Button className="mt-8 px-6 py-3 bg-black text-white hover:bg-red-200 hover:text-black rounded-xl transition duration-300">
+            <Button className="mt-10 px-6 py-3 bg-black text-white hover:bg-red-200 hover:text-black rounded-xl transition duration-300">
               View Products
             </Button>
           </Link>
