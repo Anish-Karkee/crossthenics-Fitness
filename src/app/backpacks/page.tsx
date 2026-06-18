92b313660ad065632b8bdca57f55e2a3a030901c
Anish-Karkee <anitakarkey7@gmail.com>
2026-06-18 14:21:20 +0545

    fix: adjust padding and gap for product cards components

--- src/app/backpacks/page.tsx
+++ src/app/backpacks/page.tsx
@@ -7,10 +7,10 @@ const Backpack = () => {
     return (
         <section className="w-full py-36 px-4 md:px-6 flex   bg-gray-300">
             <div className="max-w-6xl mx-auto  text-black">
-                <div className="w-full flex grid md:grid-cols-2 lg:grid-cols-3 gap-6">
+                <div className="w-full flex grid grid-cols-2 md:grid-cols-3 gap-4 md:gap-6">
                     {bagData.map((product) => (
-                        <Card key={product.id} className="p-6 rounded-2xl bg-white/10 backdrop-blur-md border border-white/20 shadow-lg shadow-black/10  transition-all  duration-300  hover:bg-white/15  hover:scale-[1.02]   hover:shadow-xl ">
-                            <div className="mt-4 p-6 rounded-lg ">
+                        <Card key={product.id} className="p-3 md:p-4 rounded-2xl bg-white/10 backdrop-blur-md border border-white/20 shadow-lg shadow-black/10  transition-all  duration-300  hover:bg-white/15  hover:scale-[1.02]   hover:shadow-xl ">
+                            <div className="mt-2 p-2 md:p-3 rounded-lg">
                                 <Image
                                     src={product.image}
                                     alt={product.name}
